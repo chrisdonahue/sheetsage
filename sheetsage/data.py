1a1210eb97a9bb6b3cc40c1da25681802dc31ee0
Chris Donahue <christopherdonahue@gmail.com>
2022-06-20 12:13:09 -0400

    Adds conversion to and from MIDI

--- sheetsage/data.py
+++ sheetsage/data.py
@@ -1,11 +1,17 @@
 import gzip
 import json
+import tempfile
 from enum import Enum
+from io import BytesIO
+
+import pretty_midi
 
 from .align import create_beat_to_time_fn
 from .assets import retrieve_asset
 
-_EPS = 1e-6
+_TICKS_PER_SECOND = 4096
+_QUANTIZE = lambda t: round(t * _TICKS_PER_SECOND) / _TICKS_PER_SECOND
+_SEGMENT_MIDI_PITCH = 75
 
 
 class Split(Enum):
@@ -25,48 +31,121 @@ class HooktheoryAlignment(Enum):
 
 class Note:
     def __init__(self, onset, pitch, offset=None):
+        if not isinstance(onset, float):
+            raise TypeError()
+        if not isinstance(pitch, int):
+            raise TypeError()
+        if offset is not None and not isinstance(offset, float):
+            raise TypeError()
         if onset < 0:
-            raise ValueError()
+            raise ValueError("Onset is negative")
         if offset is not None and offset <= onset:
-            raise ValueError()
+            raise ValueError("Offset is before onset")
         if pitch < 0 or pitch >= 128:
-            raise ValueError()
-        self.onset = onset
+            raise ValueError("Pitch is outside of MIDI range")
+        self.onset = _QUANTIZE(onset)
         self.pitch = pitch
-        self.offset = offset
+        self.offset = None if offset is None else _QUANTIZE(offset)
 
 
 class MelodyTranscriptionExample:
-    def __init__(self, uid, audio_tag, segment_start, segment_end, melody):
+    def __init__(self, segment_start, segment_end, melody, uid=None, audio_tag=None):
+        if not isinstance(segment_start, float):
+            raise TypeError()
+        if not isinstance(segment_end, float):
+            raise TypeError()
+        if not all(isinstance(n, Note) for n in melody):
+            raise TypeError()
         if segment_start < 0:
-            raise ValueError()
+            raise ValueError("Segment start is negative")
         if segment_end <= segment_start:
-            raise ValueError()
+            raise ValueError("Segment end before segment start")
+
+        segment_start = _QUANTIZE(segment_start)
+        segment_end = _QUANTIZE(segment_end)
 
         melody = sorted(melody, key=lambda n: (n.onset, n.pitch, n.offset))
-        if any(
-            (n.onset < segment_start - _EPS or n.onset > segment_end + _EPS)
-            for n in melody
-        ):
-            raise ValueError()
+        if any((n.onset < segment_start or n.onset > segment_end) for n in melody):
+            raise ValueError("Onset outside of segment range")
         if any(
             n.offset is not None
-            and (n.offset < segment_start - _EPS or n.offset > segment_end + _EPS)
+            and (n.offset < segment_start or n.offset > segment_end)
             for n in melody
         ):
-            raise ValueError(uid)
+            raise ValueError("Offset outside of segment range")
         for i in range(len(melody) - 1):
-            if (
-                melody[i].offset is not None
-                and melody[i].offset > melody[i + 1].onset + _EPS
-            ):
-                raise ValueError(uid)
+            if melody[i].onset == melody[i + 1].onset:
+                raise ValueError("Simultaneous onsets detected")
+            if melody[i].offset is not None and melody[i].offset > melody[i + 1].onset:
+                raise ValueError("Notes are not monophonic")
 
-        self.uid = uid
-        self.audio_tag = audio_tag
         self.segment_start = segment_start
         self.segment_end = segment_end
         self.melody = melody
+        self.uid = uid
+        self.audio_tag = audio_tag
+
+    @classmethod
+    def from_midi(cls, midi, segment_start=None, segment_end=None):
+        if isinstance(midi, bytes):
+            midi = pretty_midi.PrettyMIDI(BytesIO(midi))
+        elif isinstance(midi, bytes):
+            midi = pretty_midi.PrettyMIDI(midi)
+        elif isinstance(midi, pretty_midi.PrettyMIDI):
+            pass
+        else:
+            raise TypeError()
+
+        segment = []
+        melody = []
+        for i in midi.instruments:
+            for n in i.notes:
+                if i.is_drum and n.pitch == _SEGMENT_MIDI_PITCH:
+                    segment.append(n.start)
+                elif not i.is_drum:
+                    melody.append(Note(onset=n.start, pitch=n.pitch, offset=n.end))
+
+        if segment_start is None or segment_end is None:
+            if len(segment) != 2:
+                raise ValueError("Unknown segment")
+            segment_start, segment_end = sorted(segment)
+
+        return cls(segment_start=segment_start, segment_end=segment_end, melody=melody)
+
+    def to_midi(self, velocity=100):
+        midi = pretty_midi.PrettyMIDI(resolution=_TICKS_PER_SECOND, initial_tempo=60.0)
+
+        segment = pretty_midi.Instrument(0, is_drum=True, name="SEGMENT")
+        for t in [self.segment_start, self.segment_end]:
+            segment.notes.append(
+                pretty_midi.Note(
+                    start=t,
+                    end=t + (1 / _TICKS_PER_SECOND),
+                    pitch=_SEGMENT_MIDI_PITCH,
+                    velocity=127,
+                )
+            )
+
+        melody = pretty_midi.Instrument(0, name="MELODY")
+        for i, n in enumerate(self.melody):
+            offset = n.offset
+            if offset is None:
+                try:
+                    offset = self.melody[i + 1].onset
+                except IndexError:
+                    offset = n.onset + 1
+            melody.notes.append(
+                pretty_midi.Note(
+                    start=n.onset, end=offset, pitch=n.pitch, velocity=velocity
+                )
+            )
+
+        midi.instruments = [segment, melody]
+
+        with tempfile.NamedTemporaryFile() as f:
+            midi.write(f.name)
+            with open(f.name, "rb") as f:
+                return f.read()
 
 
 _CONFIG_TO_TAGS = {
--- sheetsage/data_test.py
+++ sheetsage/data_test.py
@@ -1,21 +1,103 @@
 import unittest
 
-from tqdm import tqdm
+import pretty_midi
 
-from .data import iter_hooktheory, load_hooktheory_raw
+from .data import MelodyTranscriptionExample, Note, iter_hooktheory, load_hooktheory_raw
+from .utils import compute_checksum
 
 
 class TestData(unittest.TestCase):
+    def test_note(self):
+        Note(0.0, 0)
+        Note(0.0, 60)
+        Note(0.0, 127)
+        Note(0.0, 60, 1.0)
+        with self.assertRaises(TypeError):
+            Note(0, 60)
+        with self.assertRaises(TypeError):
+            Note(0.0, 60.0)
+        with self.assertRaises(TypeError):
+            Note(0.0, 60, 1)
+        with self.assertRaises(ValueError):
+            Note(-1.0, 60)
+        with self.assertRaises(ValueError):
+            Note(1.0, 60, 0.0)
+        with self.assertRaises(ValueError):
+            Note(0.0, -1)
+        with self.assertRaises(ValueError):
+            Note(0.0, 128)
+
+    def test_melody_transcription_example(self):
+        notes = [
+            [0, 60, 1],
+            [1, 62, 2],
+            [2, 64, 3],
+            [3, 65, 4],
+            [4, 67, 5],
+        ]
+        melody = [Note(float(on), p, float(off)) for on, p, off in notes]
+        segment_start = melody[0].onset
+        segment_end = melody[-1].offset
+        MelodyTranscriptionExample(segment_start, segment_end, [])
+        e = MelodyTranscriptionExample(segment_start, segment_end, melody)
+
+        # Check MIDI round trip equivalence
+        e_bytes = e.to_midi()
+        e_rt = MelodyTranscriptionExample.from_midi(e_bytes)
+        self.assertEqual(e_rt.to_midi(), e_bytes)
+
+        # Check no offsets
+        melody_no_offsets = [Note(float(on), p) for on, p, off in notes]
+        MelodyTranscriptionExample(segment_start, segment_end, melody_no_offsets)
+        MelodyTranscriptionExample(segment_start, melody[-1].onset, melody_no_offsets)
+
+        # Check error handling
+        with self.assertRaises(TypeError):
+            MelodyTranscriptionExample(int(segment_start), segment_end, melody)
+        with self.assertRaises(TypeError):
+            MelodyTranscriptionExample(segment_start, int(segment_end), melody)
+        with self.assertRaises(TypeError):
+            MelodyTranscriptionExample(segment_start, segment_end, [None])
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(-1.0, segment_end, melody)
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(segment_end, segment_start, melody)
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(melody[1].onset, segment_end, melody)
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(segment_start, melody[-2].onset, melody)
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(segment_start, melody[-1].onset, melody)
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(
+                segment_start, segment_end, [Note(0.0, 60), Note(0.0, 60)]
+            )
+        with self.assertRaises(ValueError):
+            MelodyTranscriptionExample(
+                segment_start, segment_end, [Note(0.0, 60, 1.0), Note(0.5, 62, 1.5)]
+            )
+
     def test_hooktheory(self):
+        pretty_midi.pretty_midi.MAX_TICK = 1e8
+
+        # Test Hooktheory w/ refined (default) alignments
         hooktheory_raw = load_hooktheory_raw()
         self.assertEqual(len(hooktheory_raw), 16373)
-
         hooktheory = list(iter_hooktheory())
         self.assertEqual(len(hooktheory), 16373)
+        for e in hooktheory:
+            e_bytes = e.to_midi()
+            e_rt = MelodyTranscriptionExample.from_midi(e_bytes)
+            self.assertEqual(e_rt.to_midi(), e_bytes)
         hooktheory_test = list(iter_hooktheory(split="TEST"))
         self.assertEqual(len(hooktheory_test), 1480)
 
+        # Test Hooktheory w/ user (crude) alignments
         hooktheory_raw = load_hooktheory_raw(alignment="USER")
         self.assertEqual(len(hooktheory_raw), 20112)
         hooktheory = list(iter_hooktheory(alignment="USER"))
         self.assertEqual(len(hooktheory), 20112)
+        for e in hooktheory:
+            e_bytes = e.to_midi()
+            e_rt = MelodyTranscriptionExample.from_midi(e_bytes)
+            self.assertEqual(e_rt.to_midi(), e_bytes)
