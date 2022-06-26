72b97b6bc32c082983abf1365c139a229c255382
Chris Donahue <christopherdonahue@gmail.com>
2022-06-26 16:59:38 -0400

    Adds util methods related to audio IO

--- sheetsage/utils.py
+++ sheetsage/utils.py
@@ -3,6 +3,13 @@ import hashlib
 import pathlib
 import shlex
 import subprocess
+import tempfile
+import warnings
+
+import audioread
+import librosa
+import numpy as np
+from scipy.io.wavfile import write as wavwrite
 
 
 def compute_checksum(path_or_bytes, algorithm="sha256", gunzip=False, chunk_size=4096):
@@ -107,3 +114,176 @@ def run_cmd_sync(cmd, cwd=None, interactive=False, timeout=None):
         result = (result, stdout, stderr)
 
     return result
+
+
+_RETRIEVE_AUDIO_CMD_TEMPLATE = """
+youtube-dl \
+    --no-cache-dir \
+    --no-continue \
+    --no-playlist \
+    --format bestaudio/best \
+    {url}
+"""
+
+
+def retrieve_audio_bytes(url, return_name=False, timeout=60.0):
+    """Retrieves encoded audio (as raw bytes) from specified URL.
+
+    Parameters
+    ----------
+    url: str
+       The URL to retrieve from.
+    return_name: bool
+       If True, return the retrieved file name.
+    timeout : float
+       Max amount of time to wait before throwing an error.
+
+    Returns
+    -------
+    bytes
+       The raw bytes of the encoded audio file.
+    str, optional
+       The name of the encoded audio file, if return_name is True.
+
+    Raises
+    ------
+    :class:`subprocess.TimeoutExpired`
+       Specified timeout expired.
+    Exception
+       Error during retrieval.
+    """
+    with tempfile.TemporaryDirectory() as d:
+        assert len(list(pathlib.Path(d).iterdir())) == 0
+        status, stdout, stderr = run_cmd_sync(
+            cmd=_RETRIEVE_AUDIO_CMD_TEMPLATE.format(url=url.strip()),
+            cwd=d,
+            timeout=timeout,
+        )
+        if status != 0:
+            raise Exception(f"Failed to retrieve from {url}:\n{stderr}")
+        paths = list(pathlib.Path(d).iterdir())
+        assert len(paths) == 1
+        path = paths[0]
+        with open(path, "rb") as f:
+            audio_bytes = f.read()
+        if return_name:
+            result = (audio_bytes, path.name)
+        else:
+            result = audio_bytes
+        return result
+
+
+def decode_audio(
+    path_or_bytes,
+    sr=None,
+    offset=0.0,
+    duration=None,
+    mono=False,
+    normalize=False,
+    res_type="kaiser_best",
+):
+    """Decodes encoded audio from path or raw bytes.
+
+    Parameters
+    ----------
+    path_or_bytes: :class:`pathlib.Path`, str, or bytes
+       The filepath or raw bytes to decode.
+    sr: int
+       If specified, resample audio to this sample rate.
+    offset: float
+       Decode audio starting from this timestamp in seconds.
+    duration: float
+       Decode at most this many audio in seconds.
+    mono: bool
+       If True, average multichannel audio to mono.
+    normalize: bool
+       If True, normalize audio to max(abs(audio)) == 1.0.
+    res_type: str
+       The resampling algorithm to use (see `librosa.load` documentation).
+
+    Returns
+    -------
+    int
+       The sample rate of the decoded audio.
+    :class:`np.ndarray`
+       A NumPy array of the audio (shape [nsamps, nch], dtype float32).
+
+    Raises
+    ------
+    :class:`FileNotFoundError`
+       Unknown file.
+    :class:`RuntimeError`
+       Unknown file format.
+    """
+    with tempfile.NamedTemporaryFile("wb") as f:
+        # NOTE: This could be BytesIO but librosa has buggy support.
+        if isinstance(path_or_bytes, bytes):
+            f.write(path_or_bytes)
+            f.flush()
+            path = f.name
+        else:
+            path = path_or_bytes
+
+        # Decode audio file
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                audio, sr = librosa.load(
+                    path,
+                    sr=sr,
+                    mono=mono,
+                    offset=offset,
+                    duration=duration,
+                    res_type=res_type,
+                )
+        except audioread.exceptions.NoBackendError as e:
+            raise RuntimeError("Unknown format") from e
+
+    # Check output and rearrange to [nsamps, nch]
+    assert isinstance(sr, int)
+    assert audio.dtype == np.float32
+    assert audio.ndim in [1, 2]
+    if audio.ndim == 1:
+        audio = audio[np.newaxis, :]
+    assert audio.shape[0] > 0
+    audio = np.swapaxes(audio, 0, 1)
+    audio = np.asfortranarray(audio)
+
+    # Normalize
+    if normalize and audio.shape[0] > 0:
+        norm_factor = np.abs(audio).max()
+        if norm_factor > 0:
+            audio /= norm_factor
+
+    return sr, audio
+
+
+def encode_audio(path, sr, audio, bitexact=False, timeout=60.0):
+    """Encodes raw audio array into different file formats using FFmpeg.
+
+    Parameters
+    ----------
+    path: :class:`pathlib.Path`, str
+       Destination filepath (where the extension determines the codec).
+    sr: int
+       The sample rate of the audio.
+    audio: :class:`np.ndarray`
+       A NumPy array of the audio (shape [nsamps, nch], dtype float32).
+    bitexact: bool
+       If True, ensure reproducible checksums of output file (only on FFmpeg 4+).
+    timeout: float
+       Maximum amount of time to wait for encoding.
+
+    Raises
+    :class:`subprocess.TimeoutExpired`
+       Specified timeout expired.
+    :class:`Exception`
+       FFmpeg threw an error while encoding.
+    """
+    with tempfile.NamedTemporaryFile(suffix=".wav") as f:
+        wavwrite(f.name, sr, audio)
+        cmd = f"ffmpeg -v error -i {f.name} -y {'-bitexact' if bitexact else ''} {path}"
+        status, stdout, stderr = run_cmd_sync(cmd, timeout=timeout)
+        if status != 0:
+            raise Exception(f"FFmpeg failed: {stderr}")
+        assert pathlib.Path(path).is_file()
--- sheetsage/utils_test.py
+++ sheetsage/utils_test.py
@@ -4,7 +4,27 @@ import subprocess
 import tempfile
 import unittest
 
-from .utils import compute_checksum, run_cmd_sync
+import librosa
+import numpy as np
+
+from .assets import retrieve_asset
+from .utils import (
+    compute_checksum,
+    decode_audio,
+    encode_audio,
+    retrieve_audio_bytes,
+    run_cmd_sync,
+)
+
+
+def _quick_spec(sr, audio):
+    assert sr % 22050 == 0
+    mel = librosa.feature.melspectrogram(
+        y=audio[:, 0], sr=sr, hop_length=sr // 22050 * 512, fmax=11025
+    )
+    assert mel.max() < 300.0
+    lmel = librosa.power_to_db(mel, ref=300.0)
+    return lmel
 
 
 class TestUtils(unittest.TestCase):
@@ -60,3 +80,121 @@ class TestUtils(unittest.TestCase):
             run_cmd_sync("ls", cwd=pathlib.Path(__file__).resolve())
         with self.assertRaises(subprocess.TimeoutExpired):
             run_cmd_sync("sleep 1", timeout=1e-3)
+
+    def test_retrieve_audio_bytes(self):
+        # NOTE: CC-BY as of 22-06-26.
+        # Thanks to uploader 2-Minute Design: https://www.youtube.com/c/2-minutedesign
+        youtube_url = "https://www.youtube.com/watch?v=PPu1ekDSKw8"
+        audio_bytes, name = retrieve_audio_bytes(youtube_url, return_name=True)
+        self.assertEqual(len(audio_bytes), 136212)
+        self.assertEqual(
+            compute_checksum(audio_bytes),
+            "9c219e41c223e54642c14fe5eadf27bc792d8ce17baae993461d82639827093f",
+        )
+        self.assertEqual(
+            name, "10-Second Timer _ Red Digital On White Background [PPu1ekDSKw8].webm"
+        )
+        sr, audio = decode_audio(audio_bytes)
+        self.assertEqual(sr, 48000)
+        self.assertEqual(audio.shape, (553133, 2))
+        with self.assertRaises(subprocess.TimeoutExpired):
+            retrieve_audio_bytes(youtube_url, timeout=1e-3)
+        with self.assertRaisesRegex(Exception, "Unsupported URL"):
+            retrieve_audio_bytes("https://www.chrisdonahue.com")
+
+    def test_decode_audio(self):
+        # Load uncompressed WAV
+        raw_sr, raw_audio = decode_audio(retrieve_asset("TEST_WAV"))
+        self.assertEqual(raw_sr, 44100)
+        self.assertEqual(raw_audio.dtype, np.float32)
+        self.assertEqual(raw_audio.shape, (202311, 2))
+        self.assertAlmostEqual(np.abs(raw_audio).max(), 0.29, places=2)
+
+        # Load compressed MP3
+        mp3_path = retrieve_asset("TEST_MP3")
+        enc_sr, enc_audio = decode_audio(mp3_path)
+        self.assertEqual(enc_sr, 22050)
+        self.assertEqual(enc_audio.dtype, np.float32)
+        self.assertEqual(enc_audio.shape, (101155, 2))
+        self.assertAlmostEqual(np.abs(enc_audio).max(), 0.29, places=2)
+
+        # Ensure they're more or less the same audio
+        self.assertAlmostEqual(
+            np.abs(
+                _quick_spec(raw_sr, raw_audio) - _quick_spec(enc_sr, enc_audio)
+            ).mean(),
+            2.2,
+            places=1,
+        )
+
+        # Test bytes loading
+        with open(mp3_path, "rb") as f:
+            audio_bytes = f.read()
+        sr, audio = decode_audio(audio_bytes)
+        self.assertTrue(np.array_equal(audio, enc_audio))
+
+        # Test resampling
+        sr, audio = decode_audio(mp3_path, sr=44100)
+        self.assertEqual(sr, 44100)
+        self.assertEqual(audio.shape, (202310, 2))
+
+        # Test offset / duration limiting
+        sr, audio = decode_audio(mp3_path, offset=2, duration=1)
+        self.assertEqual(sr, 22050)
+        self.assertEqual(audio.shape, (22050, 2))
+        self.assertAlmostEqual(np.abs(audio).max(), 0.21, places=2)
+
+        # Test out of bounds offsets
+        sr, audio = decode_audio(mp3_path, offset=-1)
+        self.assertEqual(audio.shape[0], 101155)
+        sr, audio = decode_audio(mp3_path, offset=10)
+        self.assertEqual(audio.shape[0], 0)
+
+        # Test zero duration
+        sr, audio = decode_audio(mp3_path, duration=0)
+        self.assertEqual(audio.shape[0], 0)
+
+        # Test mono
+        sr, audio = decode_audio(mp3_path, mono=True)
+        self.assertEqual(audio.shape, (101155, 1))
+
+        # Test normalize
+        sr, audio = decode_audio(mp3_path, normalize=True)
+        self.assertAlmostEqual(np.abs(audio).max(), 1.0)
+
+        # Test error handling
+        with tempfile.NamedTemporaryFile() as f:
+            with self.assertRaises(FileNotFoundError):
+                decode_audio(f.name + ".noexist")
+            with open(f.name, "w") as f:
+                f.write("Text data cannot be decoded as audio.")
+            with self.assertRaisesRegex(RuntimeError, "Unknown format"):
+                decode_audio(f.name)
+
+    def test_encode_audio(self):
+        sr, audio = decode_audio(retrieve_asset("TEST_MP3"))
+        for codec, expected_err in zip([".ogg", ".wav"], [1.24, 0]):
+            with tempfile.NamedTemporaryFile(suffix=codec) as f:
+                encode_audio(f.name, sr, audio)
+                sr_hat, audio_hat = decode_audio(f.name)
+            self.assertEqual(sr_hat, sr)
+            self.assertEqual(audio.shape, (101155, 2))
+            self.assertEqual(audio_hat.shape, (101155, 2))
+            self.assertAlmostEqual(
+                np.abs(_quick_spec(sr, audio) - _quick_spec(sr, audio_hat)).mean(),
+                expected_err,
+                places=2,
+            )
+        with tempfile.NamedTemporaryFile(suffix=".ogg") as f:
+            encode_audio(f.name, sr, np.zeros((0, 1), dtype=np.float32))
+            sr, audio = decode_audio(f.name)
+            self.assertEqual(audio.shape, (0, 1))
+        with tempfile.NamedTemporaryFile(suffix=".ogg") as f:
+            with self.assertRaises(subprocess.TimeoutExpired):
+                encode_audio(f.name, sr, audio, timeout=1e-4)
+        with tempfile.TemporaryDirectory() as d:
+            with self.assertRaisesRegex(Exception, "No such file or directory"):
+                encode_audio(d + "/doesnotexist/test.wav", sr, audio)
+        with tempfile.NamedTemporaryFile(suffix=".unknowncodec") as f:
+            with self.assertRaisesRegex(Exception, "suitable output format"):
+                encode_audio(f.name, sr, audio)
