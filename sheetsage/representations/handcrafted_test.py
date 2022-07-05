d6af372c653f6f0bf9aaeb866a4b6badd2ecc189
Chris Donahue <christopherdonahue@gmail.com>
2022-07-05 17:16:36 -0400

    Adds O&F Melspec

---
+++ sheetsage/representations/handcrafted_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+import numpy as np
+
+from ..assets import retrieve_asset
+from .handcrafted import OAFMelSpec
+
+
+class TestHandcrafted(unittest.TestCase):
+    def test_oaf_transcription(self):
+        mp3_path = retrieve_asset("TEST_MP3")
+        rate, rep = OAFMelSpec().extract(mp3_path)
+        self.assertAlmostEqual(rate, 31.25, places=2)
+        self.assertEqual(rep.shape, (144, 229))
+        ref = np.load(retrieve_asset("TEST_MP3_OAFMELSPEC_REF")).T
+        err = np.abs(rep - ref)
+        self.assertLess(err.mean(), 0.015)
