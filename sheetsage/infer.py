74c6ef0e0bfefc45ae4b415fabf2d147d221a613
Chris Donahue <christopherdonahue@gmail.com>
2022-08-31 23:48:02 -0400

    Small fix

--- sheetsage/infer.py
+++ sheetsage/infer.py
@@ -251,7 +251,10 @@ def _split_into_chunks(
     for b in range(segment_start_downbeat, segment_end_beat, beats_per_chunk):
         chunk_start_tertiary = b * _TERTIARIES_PER_BEAT
         chunk_end_tertiary = ((b + beats_per_chunk) * _TERTIARIES_PER_BEAT) + 1
-        chunk_end_tertiary = min(chunk_end_tertiary, tertiaries_times.shape[0])
+        chunk_end_tertiary = min(
+            chunk_end_tertiary, (segment_end_beat * _TERTIARIES_PER_BEAT) + 1
+        )
+        assert chunk_end_tertiary <= tertiaries_times.shape[0]
         chunk_slice = slice(chunk_start_tertiary, chunk_end_tertiary)
         chunk_tertiaries_times = tertiaries_times[chunk_slice]
         duration = chunk_tertiaries_times[-1] - chunk_tertiaries_times[0]
